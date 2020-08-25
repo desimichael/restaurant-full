c578964b6f150b5c8dfecd6b43d1b06cf3572c16
desi <desimichael@desis-MacBook-Pro.local>
2020-08-25 17:35:40 -0400

    Created the admin food items pages.

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
 
+    public function home() {
+        return view('home');
+    }
     public function menu() {
         return view('menu/index');
     }
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,14 @@ Route::get('/admin', 'admin\AdminController@dashboard');
 Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
 Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
 Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
+
+// FOOD ITEMS 
+Route::get('/admin/food-items', 'admin\FoodItemsController@index');
+Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
+Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+
+
+
 // ADMIN AUTHENTICATION
 Route::get('/admin/register', function () {
     return view('admin/register');
