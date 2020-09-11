19af5993380e5260a2789bedfb605d7ed81375d7
desi <desimichael@desis-MacBook-Pro.local>
2020-09-11 13:08:44 -0400

    Added first and last names to users table

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,9 +46,18 @@
             </div>
             <div class="card-body">
                 <div class="form-group">
-                    <input id="name" type="text" class="form-control form-control-lg @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="Name" autofocus placeholder="Name">
+                    <input id="fname" type="text" class="form-control form-control-lg @error('fname') is-invalid @enderror" name="fname" value="{{ old('fname') }}" required autocomplete="Name" autofocus placeholder="First Name">
 
-                    @error('name')
+                    @error('fname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <input id="lname" type="text" class="form-control form-control-lg @error('lname') is-invalid @enderror" name="lname" value="{{ old('lname') }}" required autocomplete="Name" autofocus placeholder="Last Name">
+
+                    @error('lname')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
