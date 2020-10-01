7ca4924d070bc64c38e3ffb3ba1d533599b8aae6
desi <desimichael@desis-MacBook-Pro.local>
2020-10-01 12:08:11 -0400

    Saved the settings for SEO and Social.

--- app/Http/Controllers/admin/SettingController.php
+++ app/Http/Controllers/admin/SettingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\GeneralSetting;
+use App\SeoSetting;
+use App\SocialSetting;
 
 class SettingController extends Controller
 {
@@ -43,5 +45,54 @@ class SettingController extends Controller
         $general_setting->save();
 
         return redirect('/admin/settings/general');
+    }
+
+
+    public function seo() {
+        $id = 1;
+        $seo_setting = SeoSetting::find($id);
+
+        return view('admin/settings/seo', [
+            'seo_setting' => $seo_setting
+        ]);
+    }
+
+    public function saveSeo() {
+        $id = 1;
+        request()->validate([
+            'description' => ['required', 'string'],
+            'keywords' => ['required', 'string']
+            ]);
+        $seo_setting = SeoSetting::find($id);
+        $seo_setting->description = request('description');
+        $seo_setting->keywords = request('keywords');
+        $seo_setting->save();
+
+        return redirect('/admin/settings/seo');
+    }
+
+    public function social() {
+        $id = 1;
+        $social_setting = SocialSetting::find($id);
+
+        return view('admin/settings/social', [
+            'social_setting' => $social_setting
+        ]);
+    }
+
+    public function saveSocial() {
+        $id = 1;
+        request()->validate([
+            'facebook_url' => ['string'],
+            'twitter_url' => ['string'],
+            'instagram_url' => ['string']
+            ]);
+        $social_setting = SocialSetting::find($id);
+        $social_setting->facebook_url = request('facebook_url');
+        $social_setting->twitter_url = request('twitter_url');
+        $social_setting->instagram_url = request('instagram_url');
+        $social_setting->save();
+
+        return redirect('/admin/settings/social');
     }
 }
